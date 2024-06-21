5383f331fbbff78c6573656283f61d8a9f3d625b
ThomasPicot <alphopic@gmail.com>
2024-06-21 12:28:23 +0200

    save before implementing the trigger mode on card

---
+++ lib/parameters.cu
@@ -0,0 +1,43 @@
+#include "parameters.h"
+
+// Parameters of the static tweezers
+const double center_freq = 103000000.0;
+const int N_tweezers_vertical = 71;
+const int N_tweezers_horizontal = 4;
+
+// converting frequency with position
+const double AOD_2D_freq_to_position = 5.1;
+const double tweezers_spacing = 1.559;
+double AOD_spacing = tweezers_spacing / AOD_2D_freq_to_position * 1E6;
+
+unsigned int dynamic_num[4] = {0, 0, 0, 0};
+unsigned int static_num[4] = {N_tweezers_horizontal, N_tweezers_vertical, 0, 0};
+extern const int lThreadsPerBlock = 256;
+extern const unsigned long long llSamplerate = 300000000;
+extern const double ramp_time = 0.1;
+double static_freq[4][4096] = {{}, {}, {}, {}};
+double destination_freq[4][4096] = {{}, {}, {}, {}};
+unsigned int tone_count[5];
+unsigned int dynamic_tone_count[5];
+
+void fill_frequency_arrays()
+{
+    // Horizontal frequencies (Ch0)
+    for (int i = 0; i < N_tweezers_horizontal; ++i)
+    {
+        double offset = i - N_tweezers_horizontal / 2;
+        static_freq[0][i] = center_freq + offset * AOD_spacing;
+        destination_freq[0][i] = static_freq[0][i];
+    }
+    // Vertical frequencies (Ch1)
+    for (int i = 0; i < N_tweezers_vertical; ++i)
+    {
+        double offset = i - N_tweezers_horizontal / 2;
+        static_freq[1][i] = center_freq + offset * AOD_spacing;
+        destination_freq[1][i] = static_freq[1][i];
+    }
+}
+
+
+int dynamic_list[1024] = {0, 5, 6};          // Index of tones that is to be moved
+int static_list[16384] = {1, 2, 3, 4, 7, 8}; // Index of tones that is not moved
---
+++ lib/parameters.h
@@ -0,0 +1,18 @@
+#ifndef _parameters_included_
+#define _parameters_included_
+extern unsigned int dynamic_num[4];
+extern unsigned int static_num[4];
+extern const double ramp_time;
+
+void fill_frequency_arrays();
+void print_static_frequencies();
+
+extern double static_freq[4][4096];
+extern double destination_freq[4][4096];
+extern int dynamic_list[1024];
+extern int static_list[16384];
+extern const unsigned long long  llSamplerate;
+extern const int lThreadsPerBlock;
+extern unsigned int tone_count[5];
+extern unsigned int dynamic_tone_count[5];
+#endif
